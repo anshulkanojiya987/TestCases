289945fb84e1aa5766b1130c89d935acdf099b34
anshulkanojiya987 <78802124+anshulkanojiya987@users.noreply.github.com>
2023-03-02 18:02:35 +0530

    Add cases for mudfield and mudbutton

--- UserUI.cs
+++ UserUI.cs
@@ -1,5 +1,9 @@
+using AngleSharp.Css.Dom;
+using AngleSharp.Dom;
 using BionicApp.Pages.Add_Device;
+using BionicApp.Pages.Authentication;
 using BionicAppTestRunner.BionicApp;
+using Bogus.DataSets;
 using Bunit;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using MudBlazor;
@@ -10,6 +14,7 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 using Xunit;
 using static MudBlazor.CategoryTypes;
 using static MudBlazor.FilterOperator;
@@ -19,30 +24,85 @@ namespace BionicAppTestRunner.BionicAppUi
     public class UserUI : BionicAppUiTestBase 
     {
         [Fact]
-        public async void CheckLogo()
+        public void AllFields_AreDisplayed()
         {
-            await Manager.Instance.Login("tst_admin@example.com", "tst_admin_42");
             var component = RenderComponent<User>();
-            var avtar = component.FindComponent<MudAvatar>();
-         //   Assert.NotNull(avtar);
+            //var nameText = component.FindComponent<MudField>();
+            //var emailText = component.FindComponent<MudField>();
+            var logOutButton = component.FindAll("button").FirstOrDefault(b => b.TextContent == "Log out");
+            //Assert.NotNull(nameText);
+            //Assert.NotNull(emailText);
+            Assert.NotNull(logOutButton);
+        }
+       
+       
+        [Fact]
+        public void checkMudAvtar()
+        {
+            var avtar = RenderComponent<MudAvatar>(p =>
+                p.Add(x => x.Color, MudBlazor.Color.Primary)
+                 .Add(x => x.Size, MudBlazor.Size.Large)
+            );
+            Assert.NotNull(avtar);
+            Assert.Equal(MudBlazor.Color.Primary, avtar.Instance.Color);
+            Assert.Equal(MudBlazor.Size.Large, avtar.Instance.Size);
         }
 
-
         [Fact]
-        public void checkText()
+        public void checkNameMudField()
         {
-            var name = RenderComponent<MudField>(p => p.Add(x => x.Label, "Name"));
-            
+            var name = RenderComponent<MudField>(p =>
+                p.Add(x => x.Label, "Name")
+            );
+            Assert.NotNull(name.Instance.Label);
+            Assert.Equal("Name", name.Instance.Label);
+            Assert.Equal(Variant.Text, name.Instance.Variant);
         }
 
         [Fact]
-        public void check_Logout_Button() 
+        public void checkEmailMudField()
         {
-            // Arrange
-            var LogoutButton = RenderComponent<MudButton>(p => p
-                .Add(x => x.Style," height:52px; text-transform:none;")
+            var email = RenderComponent<MudField>(p =>
+                p.Add(x => x.Label, "Email")
+            );
+            Assert.NotNull(email.Instance.Label);
+            Assert.Equal("Name", email.Instance.Label);
+            Assert.Equal(Variant.Text, email.Instance.Variant);
+        }
+
+        [Fact]
+        public void checkRoleMudField()
+        {
+            var role = RenderComponent<MudField>(p =>
+                p.Add(x => x.Label, "Role")
+            );
+            Assert.NotNull(role.Instance.Label);
+            Assert.Equal("Role", role.Instance.Label);
+            Assert.Equal(Variant.Text, role.Instance.Variant);
+        }
+
+        [Fact]
+        public void checkLanguageMudText()
+        {
+            var language = RenderComponent<MudText>(p =>
+                p.Add(x => x.Align, Align.Center)
+             //   .Add(x => x.Typo, "Typo.h6")
             );
             
+        }
+
+
+        [Fact]
+        public void checkLogoutButton() 
+        {
+            // Arrange
+            var LogoutButton = RenderComponent<MudButton>(p =>
+                p.Add(x => x.Style, "height:52px; text-transform:none;")
+                .Add(x => x.Color, MudBlazor.Color.Primary)
+            );
+            Assert.NotNull(LogoutButton);
+            Assert.Equal(MudBlazor.Color.Primary,LogoutButton.Instance.Color);
+            Assert.Equal("height:52px; text-transform:none;", LogoutButton.Instance.Style);
         }
 
         [Fact]
