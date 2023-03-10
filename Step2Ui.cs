c8cf3da081c3b865b639dc62c749ffba8059bd76
anshulkanojiya987 <78802124+anshulkanojiya987@users.noreply.github.com>
2023-03-10 16:06:36 +0530

    Updated Step2

--- Step2Ui.cs
+++ Step2Ui.cs
@@ -1,15 +1,8 @@
-﻿using BionicApp.Pages.Add_Device.Steps;
+using BionicApp.Pages.Add_Device.Steps;
 using Bunit;
 using MudBlazor;
 using Ossur.Bionics.Common;
-using Plugin.BLE;
-using Plugin.BLE.Abstractions.Contracts;
-using System;
-using System.Collections.Generic;
 using System.Globalization;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Xunit;
 
 namespace BionicAppTestRunner.BionicAppUi
@@ -37,7 +30,7 @@ namespace BionicAppTestRunner.BionicAppUi
             CultureInfo.DefaultThreadCurrentUICulture = new CultureInfo("en");
             var component = RenderComponent<Step2>();
             var mudDialog = component.FindComponents<MudDialog>()[0];
-            //var DialogAction = mudDialog.FindComponent<DialogActions>();
+            Assert.Equal("enablebluetooth_ua", (mudDialog.Instance.DialogContent).ToString());
             //var DialogItem = mudDialog.FindComponent<DialogContent>();
             mudDialog.MarkupMatches("");
         }
@@ -56,8 +49,51 @@ namespace BionicAppTestRunner.BionicAppUi
             var mudGrid = component.FindComponents<MudGrid>()[0];
             var mudTtem = mudGrid.FindComponents<MudItem>()[0];
             var MudIconButton = mudTtem.FindComponent<MudIconButton>();
-            Assert.Equal("@Icons.Material.Filled.ArrowBack", MudIconButton.Instance.Icon);
+            Assert.Equal("<path d=\"M0 0h24v24H0z\" fill=\"none\"/><path d=\"M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z\"/>", MudIconButton.Instance.Icon);
             
         }
+        [Fact]
+        public async void checkMudTextInMudGrid()
+        {
+            await Manager.Instance.Login("https://bionicregistry40dev.azurewebsites.net/api/v1", "tst_admin@example.com", "tst_admin_42");
+            const string key = "TranslationCutoffDate";
+            var cutoff = Manager.Instance.GetValue(key, DateTime.MinValue);
+            await Manager.Instance.CloudSync.PullTranslationsFromCloud(cutoff, 1, 1000, "en", "USERAPP_V1.0");
+            CultureInfo.DefaultThreadCurrentUICulture = new CultureInfo("en");
+            var component = RenderComponent<Step2>();
+            var mudGrid = component.FindComponents<MudGrid>()[0];
+            var mudTtem = mudGrid.FindComponents<MudItem>()[1];
+            var mudText = mudTtem.FindComponent<MudText>();
+            Assert.Equal(Typo.h5, mudText.Instance.Typo);
+            Assert.Equal(Align.Center, mudText.Instance.Align);
+            mudText.MarkupMatches("<h5 class=\"mud-typography mud-typography-h5 mud-typography-alingn-center\">Add a device</h5>");
+        }
+        [Fact]
+        public async void CheckStepText()
+        {
+            var manager =await Manager.Instance.Login("https://bionicregistry40dev.azurewebsites.net/api/v1", "tst_admin@example.com", "tst_admin_42");
+            const string key = "TranslationCutoffDate";
+            var cutoff = Manager.Instance.GetValue(key, DateTime.MinValue);
+            var cloud = await Manager.Instance.CloudSync.PullTranslationsFromCloud(cutoff, 1, 1000, "en", "USERAPP_V1.0");
+            CultureInfo.DefaultThreadCurrentUICulture = new CultureInfo("en");
+            var component = RenderComponent<Step2>();
+            var mudText = component.FindComponents<MudText>()[1];
+            Assert.Equal(Typo.subtitle1,mudText.Instance.Typo);
+            mudText.MarkupMatches("<h6 class=\"mud-typography mud-typography-subtitle1\">Step 2/4</h6>");
+        }
+        [Fact]
+        public void checkrefreshButton() 
+        {
+            var component = RenderComponent<Step2>();
+            var MudIconButton = component.FindComponents<MudIconButton>()[1];
+            Assert.Equal("refresh", MudIconButton.Instance.Class);
+            var MudProgessCircu= MudIconButton.FindComponent<MudProgressCircular>();
+            Assert.Equal("ms-n1",MudProgessCircu.Instance.Class);
+            Assert.Equal(MudBlazor.Size.Small, MudProgessCircu.Instance.Size);
+            Assert.True(MudProgessCircu.Instance.Indeterminate);
+            //var MudIcon = MudIconButton.FindComponent<MudIcon>();
+            //Assert.Equal("< path d =\"M0 0h24v24H0z\" fill=\"none\"/><path d=\"M17.65 6.35C16.2 4.9 14.21 4 12 4c-4.42 0-7.99 3.58-7.99 8s3.57 8 7.99 8c3.73 0 6.84-2.55 7.73-6h-2.08c-.82 2.33-3.04 4-5.65 4-3.31 0-6-2.69-6-6s2.69-6 6-6c1.66 0 3.14.69 4.22 1.78L13 11h7V4l-2.35 2.35z\"/>", MudIcon.Instance.Icon);
+        }
+
     }
 }
